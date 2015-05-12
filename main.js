1ad81525185586e2411061eab5570912c9716dc3
zodern <zodern@users.noreply.github.com>
2015-05-11 21:19:01 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// global
+niceReload = function () {
+  Reload._reload();
+};
+
+Template.body.onCreated(function () {
+  var ctrlDown = false;
+  var ctrlKey = 17;
+
+  $(document).keydown(function(e)
+  {
+    if (e.keyCode == ctrlKey) ctrlDown = true;
+  }).keyup(function(e)
+  {
+    if (e.keyCode == ctrlKey) ctrlDown = false;
+  });
+
+  $(document).keydown(function(e) {
+    console.log(ctrlDown, e.keyCode);
+    if (ctrlDown && e.keyCode === 78) {
+      niceReload();
+    }
+  });
+});
